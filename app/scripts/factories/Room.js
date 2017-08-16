a08f29a8fd5beac560bb9c84d8f921843c1e7546
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2017-08-15 20:36:42 -0700

    Room factory add firebase

--- app/scripts/factories/Room.js
+++ app/scripts/factories/Room.js
@@ -1,6 +1,10 @@
 (function() {
     function Room($firebaseArray) {
-        var ref = firebase.database().ref();
+        var Room = {};
+        var ref = firebase.database().ref().child("rooms");
+        var rooms = $firebaseArray(ref);
+
+        Room.all = rooms;
     }
 
     angular
